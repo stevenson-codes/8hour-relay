e558956f987e721a8fc493f79b79a24a0800552e
Stevenson Yan <stevensoncodes@gmail.com>
2026-08-21 03:56:16 -0700

    sprint boot initial commit

---
+++ backend/src/main/java/com/sbeve/relaytiming/RelaytimingApplication.java
@@ -0,0 +1,13 @@
+package com.sbeve.relaytiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaytimingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaytimingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sbeve/relaytiming/RelaytimingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbeve.relaytiming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelaytimingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
